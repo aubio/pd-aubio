b7267da7af05d0dbe702e6458bee72eb12fc7965
Paul Brossier <piem@piem.org>
2017-03-14 03:54:06 +0100

    scripts/get_waf.sh: add script to fetch waf

---
+++ scripts/get_waf.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+set -e
+set -x
+
+WAFURL=https://waf.io/waf-1.9.6
+
+( which wget > /dev/null && wget -qO waf $WAFURL ) || ( which curl > /dev/null && curl $WAFURL > waf )
+
+chmod +x waf
+
